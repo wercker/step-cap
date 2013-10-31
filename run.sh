0597aedd1a5fcc6adbdd51319f6db5eeda8d280b
Benno van den Berg <bennovandenberg@gmail.com>
2013-10-31 15:45:33 +0100

    Remove PATH use cwd instead

--- run.sh
+++ run.sh
@@ -9,7 +9,6 @@
 # fi
 
 cap_command="bundle exec cap"
-cap_working_path=""
 
 # Parse some variable arguments
 if [ -n "$WERCKER_CAP_STAGE" ] ; then
@@ -22,28 +21,12 @@ else
     cap_command="$cap_command deploy"
 fi
 
-# Figure out the working directory
-if [ -n "$WERCKER_CAP_PATH" ] ; then
-  cap_working_path="$WERCKER_CAP_PATH"
+debug "$cap_command"
+$cap_command
+
+if [[ $? -ne 0 ]]; then
+    warn "$result"
+    fail "cap command failed"
 else
-  cap_working_path="$WERCKER_SOURCE_DIR"
-fi
-
-# Stop processing if the working path was not found
-if [ ! -d "$cap_working_path" ] ; then
-    fail "Unable to continue; Working directory was not found: $cap_working_path"
-else
-    info "Switching to path: $cap_working_path"
-    cd $cap_working_path
-
-    debug "$cap_command"
-    $cap_command
-
-    if [[ $? -ne 0 ]]
-    then
-        warn "$result"
-        fail "cap command failed"
-    else
-        success "finished $cap_command"
-    fi
+    success "finished $cap_command"
 fi
